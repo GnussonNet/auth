a8251f2f88860326c1025c688afb1452660e2646
Gnusson.net <admin@gnusson.net>
2022-08-31 14:00:58 +0200

    👌 IMPROVE: Switched to component buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import styles from '../styles/modules/Home.module.scss';
 import { useSession } from 'next-auth/react';
 import { Layout } from '../components/Layout/Layout';
+import { PrimaryButton, SecondaryButton } from '../components/Buttons/Buttons';
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
@@ -16,8 +17,8 @@ const Home: NextPage = () => {
               <p className={styles.description}>GnussonNet is a Decentralized solution to Credentials Authentications; where Users always hold the private key of their account, the credentials match and are always valid. This protects Users from many attacks and phishing attempts.</p>
 
               <div className={styles.buttons}>
-                <input className={`${styles.button} ${styles['button--primary_button']}`} type="button" value="Create free account" />
-                <input className={`${styles.button} ${styles['button--secondary_button']}`} type="button" value="Learn more" />
+                <PrimaryButton title="Create free account" />
+                <SecondaryButton title="Learn more" />
               </div>
             </div>
             <div className={styles.landing_page__right}></div>
