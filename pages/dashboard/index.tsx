0978aeb9697f635cb509c76be6518f7afbe6a9ec
Gnusson.net <admin@gnusson.net>
2022-08-31 20:46:49 +0200

    👌 IMPROVE: Switched to component buttons

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from 'next';
 import { signOut } from 'next-auth/react';
+import { PrimaryButton } from '../../components/Buttons/Buttons';
 import { Layout } from '../../components/Layout/Layout';
 import styles from './Dashboard.module.scss';
 
 const Dashboard: NextPage = (props): JSX.Element => {
   return (
     <div className={styles.dashboard}>
-      <button onClick={() => signOut()}>Sign Out</button>
+      <PrimaryButton onClick={() => signOut()} title="Sign Out" />
     </div>
   );
 };
