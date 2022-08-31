b1dd183d1131d6150ea26dd537a1c515163fd013
Gnusson.net <admin@gnusson.net>
2022-08-31 20:48:07 +0200

    👌 IMPROVE: Switched to component buttons

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import Link from 'next/link'
+import Link from 'next/link';
 import { useSession } from 'next-auth/react';
+import { PrimaryButton } from '../Buttons/Buttons';
 
 export default function Header() {
   const { data: session, status } = useSession();
 
-
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <h4>Gnusson Auth</h4>
+        <Link href="/">
+          <h4>Gnusson Auth</h4>
+        </Link>
       </div>
       <div className={styles.right}>
         {status === 'authenticated' ? (
           <>
-            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/dashboard">
+              <PrimaryButton title="Dashboard" />
+            </Link>
           </>
         ) : (
           <>
-            <Link href="/">Home</Link>|<Link href="/signin">Sign in</Link>|<Link href="/signup">Create free account</Link>
+            <Link href="/signup">
+              <PrimaryButton title="Create free account" />
+            </Link>
+            <Link href="/signin">Sign in</Link>
           </>
         )}
       </div>
