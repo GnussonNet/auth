cca594d320e3b507864c6bc80d93c5ab10f952b6
Gnusson.net <admin@gnusson.net>
2022-09-05 20:24:16 +0200

    📦 NEW: Show profile image

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,16 @@
 import { NextPage } from 'next';
-import { signOut } from 'next-auth/react';
-import { PrimaryButton } from '../../components/Buttons/Buttons';
-import { Layout } from '../../components/Layout/Layout';
+import { signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Buttons from '../../components/Buttons/Buttons';
 import styles from './Dashboard.module.scss';
 
 const Dashboard: NextPage = (props): JSX.Element => {
+  const { data: session, status } = useSession();
+
   return (
     <div className={styles.dashboard}>
-      <PrimaryButton onClick={() => signOut()} title="Sign Out" />
+      <Buttons type="primary" onClick={() => signOut()} title="Sign Out" />
+      {status === 'authenticated' && <Image src={session?.user?.image as string} width={30} height={30} alt="test" />}
     </div>
   );
 };
