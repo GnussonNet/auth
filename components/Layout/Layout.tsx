0bc32bee53dcb6e64a619c9f3273e86c163f4062
Gnusson.net <admin@gnusson.net>
2022-09-13 18:53:23 +0200

    👌 IMPROVE: Changed theme color and app name

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,9 +13,9 @@ export const Layout = ({ children, title }: WrapperProps) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Vintergatan" />
-        <meta name="application-name" content="Vintergatan" />
-        <meta name="apple-mobile-web-app-title" content="Vintergatan" />
+        <meta name="description" content="GAuth" />
+        <meta name="application-name" content="GAuth" />
+        <meta name="apple-mobile-web-app-title" content="GAuth" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1.0.0" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1.0.0" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1.0.0" />
@@ -24,7 +24,7 @@ export const Layout = ({ children, title }: WrapperProps) => {
         <link rel="shortcut icon" href="/favicon.ico?v=1.0.0" />
         <meta name="msapplication-TileColor" content="#5683ff" />
         <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=1.0.0" />
-        <meta name="theme-color" content="#181A1F" />
+        <meta name="theme-color" content="#121212" />
         <title>{title || 'GnussonAuth'}</title>
       </Head>
       <Header />
