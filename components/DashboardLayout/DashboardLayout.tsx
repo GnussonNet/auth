836b9719c10289cd68acc81d149e3dc08631cb12
Gnusson.net <admin@gnusson.net>
2022-09-14 13:32:25 +0200

    📦 NEW: Added separate layout for dashboard

---
+++ components/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,46 @@
+import { signOut, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import Buttons from '../Buttons/Buttons';
+import Header from '../Header/Header';
+import styles from './DashboardLayout.module.scss';
+
+interface LayoutProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+export const DashboardLayout = ({ children, title }: LayoutProps) => {
+  const { data: session, status } = useSession();
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="GAuth" />
+        <meta name="application-name" content="GAuth" />
+        <meta name="apple-mobile-web-app-title" content="GAuth" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1.0.0" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1.0.0" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1.0.0" />
+        <link rel="manifest" href="/site.webmanifest?v=1.0.0" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1.0.0" color="#5683ff" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1.0.0" />
+        <meta name="msapplication-TileColor" content="#5683ff" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=1.0.0" />
+        <meta name="theme-color" content="#121212" />
+        <title>{title || 'GnussonAuth Dashboard'}</title>
+      </Head>
+      <Header>
+        <li>
+          <Buttons type="primary" onClick={() => signOut()} title="Sign Out" />
+        </li>
+        <li>
+          <Image src={session?.user?.image as string} width={36} height={36} alt="test" />
+        </li>
+      </Header>
+      <div className={styles.container}>{children}</div>
+    </>
+  );
+};
