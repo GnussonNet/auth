92ff9632920609116a44b289a573abc1a62b3da4
Gnusson.net <admin@gnusson.net>
2022-09-15 19:21:53 +0200

    👌 IMPROVE: Added 404 image

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,6 +1,7 @@
 import { NextPage, NextPageContext } from 'next';
 import { Layout } from '@/components/Layout/Layout';
 import styles from '@/styles/modules/Error.module.scss';
+import Image from 'next/image';
 
 interface Props {
   statusCode?: number;
@@ -10,7 +11,9 @@ const Error: NextPage<Props> = ({ statusCode }) => {
   return (
     <Layout title={statusCode ? `GnussonAuth | ${statusCode}` : 'GnussonAuth | An error occurred on client'}>
       <div className={styles.container}>
+        {statusCode === 404 && <Image src="/404.svg" alt="Error" width={750} height={500} />}
         <h3>{statusCode ? `An ${statusCode} error occurred on server` : 'An error occurred on client'}</h3>
+        {statusCode === 404 && <a href="https://storyset.com/online">Online illustrations by Storyset</a>}
       </div>
     </Layout>
   );
