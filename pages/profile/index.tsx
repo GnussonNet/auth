d5c427359c6ea5ea5b45eb9e8477586cc68102c6
Gnusson.net <admin@gnusson.net>
2022-09-21 15:43:23 +0200

    👌 IMPROVE: Changed to regular layout

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,18 +1,18 @@
 import { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
-import { DashboardLayout } from '@/components/DashboardLayout/DashboardLayout';
 import styles from './Profile.module.scss';
+import { Layout } from '@/components/Layout/Layout';
 
 const Profile: NextPage = (props): JSX.Element => {
   const { data: session, status } = useSession();
 
   return status === 'authenticated' ? (
-    <DashboardLayout title="Profile">
+    <Layout title="Profile">
       <div className={styles.container}>
         <div className={styles.Profile}>
         </div>
       </div>
-    </DashboardLayout>
+    </Layout>
   ) : (
     <></>
   );
